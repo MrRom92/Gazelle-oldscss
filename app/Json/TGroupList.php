ad946fa56a44505ff2f72fc1ce95e4c97b77ba8b
itismadness <itismadness@orpheus.network>
2025-09-06 21:15:57 +0000

    Fix 500 error on ajax browse

--- app/Json/TGroupList.php
+++ app/Json/TGroupList.php
@@ -49,7 +49,7 @@ class TGroupList extends \Gazelle\Json {
                     $groupList[] = [
                         'torrentId'               => $torrent->id,
                         'editionId'               => $EditionID,
-                        'artists'                 => $tgroup->artistRole()->roleList()['main'],
+                        'artists'                 => $tgroup->artistRole()?->roleList()['main'],
                         'remastered'              => $torrent->isRemastered(),
                         'remasterYear'            => $torrent->remasterYear(),
                         'remasterRecordLabel'     => $torrent->remasterRecordLabel() ?? '',
