c7062326344fd9c8c096b150dcaa67533142e5ee
itismadness <itismadness@orpheus.network>
2025-09-09 19:11:51 +0000

    Add select perms to nyro docker user

--- misc/docker/pg/200-role-ro.sql
+++ misc/docker/pg/200-role-ro.sql
@@ -1,1 +1,8 @@
 create role nyro with password 'nyropw' login;
+grant connect on database gz to nyro;
+
+\c gz
+
+grant usage on schema public to nyro;
+grant select on all tables in schema public to nyro;
+alter default privileges in schema public grant select on tables to nyro;
