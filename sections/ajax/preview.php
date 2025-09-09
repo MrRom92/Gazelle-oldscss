34df5fa0fb54a98a538504dbc08f5c758d62fe08
Spine <spine@orpheus.network>
2025-09-09 08:09:15 +0000

    preview text with image cache

--- sections/ajax/preview.php
+++ sections/ajax/preview.php
@@ -7,9 +7,9 @@ namespace Gazelle;
 \Text::$TOC = true;
 
 if (!empty($_POST['admincomment'])) {
-    echo \Text::full_format($_POST['admincomment']);
+    echo \Text::full_format($_POST['admincomment'], cache: IMAGE_CACHE_ENABLED);
 } elseif (!empty($_POST['WikiText'])) {
-    echo \Text::full_format($_REQUEST['WikiText']);
+    echo \Text::full_format($_REQUEST['WikiText'], cache: IMAGE_CACHE_ENABLED);
 } else {
-    echo \Text::full_format($_REQUEST['body']);
+    echo \Text::full_format($_REQUEST['body'], cache: IMAGE_CACHE_ENABLED);
 }
