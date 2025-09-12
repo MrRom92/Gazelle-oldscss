cebfc2d9a7da1d0925ad32b6c6fda4c985d2f345
Spine <spine@orpheus.network>
2025-09-12 21:13:16 +0000

    tidy up PHP warning in torrents stats page

--- sections/stats/torrents.php
+++ sections/stats/torrents.php
@@ -10,10 +10,10 @@ $flow = $statsTor->flow();
 
 echo $Twig->render('stats/torrent.twig', [
     'flow' => [
-        'month' => array_values(array_map(fn($m) => $m['Month'], $flow)),
-        'add'   => array_values(array_map(fn($m) => $m['t_add'], $flow)),
-        'del'   => array_values(array_map(fn($m) => $m['t_del'], $flow)),
-        'net'   => array_values(array_map(fn($m) => $m['t_net'], $flow)),
+        'month' => array_values(array_map(fn ($m) => $m['month'], $flow)),
+        'add'   => array_values(array_map(fn ($m) => $m['t_add'], $flow)),
+        'del'   => array_values(array_map(fn ($m) => $m['t_del'], $flow)),
+        'net'   => array_values(array_map(fn ($m) => $m['t_net'], $flow)),
     ],
     'category' => $statsTor->categoryTotal(),
 ]);
